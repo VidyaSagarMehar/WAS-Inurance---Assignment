8ab797845f0cf899597331650376405be5f56032
VidyaSagarMehar <vidyasagark890@gmail.com>
2026-02-07 19:11:03 +0530

    drop down menu bug fix and hamburgger added for smaller screen

--- script.js
+++ script.js
@@ -7,12 +7,14 @@ function openDropdown() {
 	travelMenu.classList.remove('hidden');
 	overlay.classList.remove('hidden');
 	travelArrow.classList.add('rotate-180');
+	document.getElementById('main-header').classList.add('open');
 }
 
 function closeDropdown() {
 	travelMenu.classList.add('hidden');
 	overlay.classList.add('hidden');
 	travelArrow.classList.remove('rotate-180');
+	document.getElementById('main-header').classList.remove('open');
 }
 
 travelBtn.addEventListener('click', (e) => {
@@ -27,4 +29,29 @@ overlay.addEventListener('click', closeDropdown);
 // Optional: close on ESC
 document.addEventListener('keydown', (e) => {
 	if (e.key === 'Escape') closeDropdown();
+});
+
+// Hamburger Menu
+const openMobileMenu = document.getElementById('openMobileMenu');
+const closeMobileMenu = document.getElementById('closeMobileMenu');
+const mobileMenu = document.getElementById('mobileMenu');
+
+const mobileTravelBtn = document.getElementById('mobileTravelBtn');
+const mobileTravelMenu = document.getElementById('mobileTravelMenu');
+const mobileTravelArrow = document.getElementById('mobileTravelArrow');
+
+// Open mobile menu
+openMobileMenu.addEventListener('click', () => {
+	mobileMenu.classList.remove('hidden');
+});
+
+// Close mobile menu
+closeMobileMenu.addEventListener('click', () => {
+	mobileMenu.classList.add('hidden');
+});
+
+// Toggle travel submenu
+mobileTravelBtn.addEventListener('click', () => {
+	mobileTravelMenu.classList.toggle('hidden');
+	mobileTravelArrow.classList.toggle('rotate-180');
 });
