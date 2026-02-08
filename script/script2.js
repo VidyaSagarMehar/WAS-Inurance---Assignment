9bddae6d1c92c4c9fbe67a46f08d99740fb7cfc5
VidyaSagarMehar <vidyasagark890@gmail.com>
2026-02-08 13:44:01 +0530

    choosePlan page optimized for smaller screen

--- script/script2.js
+++ script/script2.js
@@ -1,3 +1,17 @@
+// get the current year for footer copyright
 let year = document.getElementById('year');
 const currentYear = new Date().getFullYear();
 year.innerHTML = currentYear;
+
+// Plan Table
+function toggleBenefits(id, btn) {
+	const section = document.getElementById(id);
+
+	if (section.classList.contains('hidden')) {
+		section.classList.remove('hidden');
+		btn.innerText = 'Hide benefits';
+	} else {
+		section.classList.add('hidden');
+		btn.innerText = 'Show all benefits';
+	}
+}
